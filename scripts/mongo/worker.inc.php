02bda6a378f569609fd4582c9f2b2710cc88a517
Grzegorz Rajchman <grzegorz.rajchman@talis.com>
2026-03-31 14:14:01 +0100

    Replace dependency on monolog/monolog with psr/log

--- scripts/mongo/worker.inc.php
+++ scripts/mongo/worker.inc.php
@@ -9,8 +9,7 @@ require_once __DIR__ . '/common.inc.php';
 require_once __DIR__ . '/../../src/tripod.inc.php';
 
 // the global is necessary for Resque worker to send statements to
-$logger = new Logger('TRIPOD-WORKER');
-$logger->pushHandler(new StreamHandler('php://stderr', LogLevel::WARNING)); // resque too chatty on NOTICE & INFO. YMMV
+$logger = new Logger('TRIPOD-WORKER', [new StreamHandler('php://stderr', LogLevel::NOTICE)]);
 
 // this is so tripod itself uses the same logger
-DriverBase::$logger = new Logger('TRIPOD-JOB', [new StreamHandler('php://stderr', LogLevel::DEBUG)]);
+DriverBase::$logger = $logger;
